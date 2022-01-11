9c78917acd1ce452744ac2fbb9fcce6faa2c4f22
Ummu-habeeba-t <74949037+Ummu-habeeba-t@users.noreply.github.com>
2022-01-11 06:30:42 +0530

    Add files via upload

---
+++ 58 Ummu Habeeba T/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.admin.decorators import register
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('register',views.register,name='register'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('user_login',views.user_login,name='user_login'),
+    path('user_logout',views.user_logout,name='user_logout'),
+]

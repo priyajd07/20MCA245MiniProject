93c7ed2d98fe62adb7866f620969e3bc6f478391
Muhammed Munees <muneesmmm@gmail.com>
2022-01-28 14:15:02 +0000

    add user module

--- 34-MUHAMMED MUNEES M/PROJECT/Medical_lab_report_generator/routes/admin.js
+++ 34-MUHAMMED MUNEES M/PROJECT/Medical_lab_report_generator/routes/admin.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render("admin/login");
 });
-
+router.get('/index', function(req, res, next) {
+  res.render("admin/index");
+});
+router.get('/patients', function(req, res, next) {
+  res.render("admin/patients");
+});
+router.post('/login',(req, res, next)=> {
+  console.log(req.body);
+  let email=req.body.email
+  let password=req.body.password
+  if (email=="muneesmmm@gmail.com" && password=="123458") {
+    res.redirect('/admin/index')
+  }else{
+    res.redirect('/')
+  }
+});
 module.exports = router;

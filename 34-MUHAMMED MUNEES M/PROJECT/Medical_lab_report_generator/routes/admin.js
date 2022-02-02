07596aeb4a503ac54267f61c677ae9a3230c60a6
Muhammed Munees <muneesmmm@gmail.com>
2022-02-02 23:35:45 +0530

    add nod mailer to send username and password to user

--- 34-MUHAMMED MUNEES M/PROJECT/Medical_lab_report_generator/routes/admin.js
+++ 34-MUHAMMED MUNEES M/PROJECT/Medical_lab_report_generator/routes/admin.js
@@ -2,14 +2,25 @@ var express = require('express');
 var router = express.Router();
 const adminHelpers = require('../helpers/adminhelper')
 const userHelpers = require('../helpers/userhelper')
-
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next()
+  } else {
+    res.redirect('/admin/login')
+  }
+}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render("admin/login");
 });
 router.get('/index', function(req, res, next) {
-  adminHelpers.send()
+  let admin = req.session.admin
+  if (req.session.loggedIn) {
     res.render("admin/index");
+  } else {
+    res.render('admin/login', { "LoginErr": req.session.LoginErr })
+    req.session.LoginErr = false
+  }
 });
 router.post('/login',(req, res, next)=> {
   console.log(req.body);
@@ -29,4 +40,13 @@ router.get('/patients', function(req, res, next) {
 router.get('/addpatients', function(req, res, next) {
   res.render("admin/addpatients");
 });
+router.post('/addpatients',async (req, res) => {
+  adminHelpers.addpatients(req.body).then((response) => {
+    console.log(response)
+    req.session.admin = response
+    req.session.loggedIn = true
+  })
+  adminHelpers.send(req.body)
+  res.redirect('/admin/patients') 
+})
 module.exports = router;

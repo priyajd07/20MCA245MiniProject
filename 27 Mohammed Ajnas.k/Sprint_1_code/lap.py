f8c64215857a798252c217201b06c9f9c826a860
Mohammed Ajnas k <75599001+Ajnas90@users.noreply.github.com>
2022-02-21 16:25:35 +0530

    Add files via upload

---
+++ 27 Mohammed Ajnas.k/Sprint_1_code/lap.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import sqlite3
+import time
+import urllib
+
+
+def a_lap(pro,cho):
+    print("hi")
+
+
+def f_lap(pro,cho):
+    print("hi")
---
+++ 27 Mohammed Ajnas.k/Sprint_1_code/tab.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import sqlite3
+import time
+import urllib
+
+
+def a_tab(pro,cho):
+    print("hi")
+
+
+def f_tab(pro,cho):
+    print("hi")
